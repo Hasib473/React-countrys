cc56290fb9d06858c393f3935b8218156cabfa0c
Hasib Al Mamun <hasib.gub221@gmail.com>
2025-10-03 16:03:41 +0600

    adding first commit on this rest country project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Countrys from './Component/countrys/countrys'
+
+function App() {
+  
+
+  return (
+    <>
+     <Countrys></Countrys>
+    </>
+  )
+}
+
+export default App
---
+++ src/Component/countrys/countrys.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const countrys = () => {
+    return (
+        <div>
+            <h2>I love My Countries</h2>
+        </div>
+    );
+};
+
+export default countrys;
