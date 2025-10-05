898e57064879ed1780d2a654f6e5b9282a819613
Hasib Al Mamun <hasib.gub221@gmail.com>
2025-10-06 02:08:51 +0600

    adding toggiling in react

--- src/Component/Country/Country.jsx
+++ src/Component/Country/Country.jsx
@@ -1,13 +1,38 @@
-import React, { use } from 'react';
+import React, {  useState } from 'react';
 
 const Country = ({country}) =>{
+
+    const [visited, setVisited] =useState(false)
+
+
+    const btnclicked =() =>{
+        {
+            visited ? setVisited(false) : setVisited(true)
+        }
+    }
+
+  
+
     const countryName =(country.name.common);
     const flagImg=(country.flags.flags.png)
+    const population =(country.population.population)
+    const countryArea =(country.area.area)
+    const countryRegion =(country.region.region)
     return (
     
-         <div className='border-2 border-green-500  m-[10px] p-[20px] rounded-2xl '>
-            <img className='w-[200px] bg-red-100 p-[20px] rounded-2xl' src={flagImg} alt="" />
+         <div  className={`m-[10px] p-[20px] rounded-2xl shadow-2xl ${
+        visited ? "bg-red-200" : "bg-green-200"
+      }`} >
+            <img className='w-auto h-auto bg-red-100 p-[20px] rounded-2xl' src={flagImg} alt="" />
             <h3 className=''>Name:{countryName}</h3>
+            <h2>Population:{population}</h2>
+            <h2>Area:{countryArea} km<sup>2</sup></h2>
+            <h2>Region:{countryRegion}</h2>
+            <button onClick={btnclicked} className='bg-amber-500 p-2 m-2 rounded-2xl w-25 text-white hover:bg-blue-500 cursor-pointer hover:text-black'>
+                {
+                    visited? 'Visited' : 'Not visited'
+                }
+            </button>
         </div>
        
     );
--- src/Component/countrys/countrys.jsx
+++ src/Component/countrys/countrys.jsx
@@ -2,17 +2,22 @@ import React, { use } from 'react';
 import Country from '../Country/Country';
 
 const Countrys = ({countriesPromiss}) => {
+
     const total = use(countriesPromiss);
     const cnti = total.countries
+
+    
     
     return (
         <>
-        <h2>totalcountry:{cnti.length}</h2>
+        <h2 className='text-3xl text-center font-bold text-blue-700 uppercase bg-orange-200 p-[20px] mb-[20px]'>total country: {cnti.length}</h2>
+        <h3 className='text-2xl font-bold uppercase text-center'>Total country visited : </h3>
         <div className=' grid grid-cols-4  '>
             
             {
                 cnti.map(country => <Country country={country}></Country>)
             }
+            
         </div>
 
         </>
